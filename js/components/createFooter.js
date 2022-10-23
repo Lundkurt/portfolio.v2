ec2a3e6b48421d7e751481774fa82708154c9704
Lundkurt <77051800+Lundkurt@users.noreply.github.com>
2022-10-23 22:05:41 +0200

    changes, footer

---
+++ js/components/createFooter.js
@@ -0,0 +1,15 @@
+export default function createFooter() {
+  const footer = document.querySelector("footer");
+
+  footer.innerHTML = `<div class="border-top">
+    <div class="footer-info">
+        <p class="white">+47 480 40 772</p>
+        <a class="white" href="https://www.discordapp.com/users/178577276315762688">Cracked#7777</a>
+        <a class="white" href="mailto: kurtien@live.no">Kurtien@live.no</a>
+    </div>
+    
+
+</div>
+
+<div class="copyright white">Copyright &#169; Kurt André Lund</div>`;
+}
--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
 import createHeader from "./components/createHeader.js";
+import createFooter from "./components/createFooter.js";
 
 createHeader();
+createFooter();
