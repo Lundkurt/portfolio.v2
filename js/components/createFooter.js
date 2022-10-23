7c988b7e5d0ccd78972d307dc66626da2f57be44
Lundkurt <77051800+Lundkurt@users.noreply.github.com>
2022-10-23 22:25:22 +0200

    fixes

--- js/components/createFooter.js
+++ js/components/createFooter.js
@@ -5,7 +5,7 @@ export default function createFooter() {
     <div class="footer-info">
         <p class="white">+47 480 40 772</p>
         <a class="white" href="https://www.discordapp.com/users/178577276315762688">Cracked#7777</a>
-        <a class="white" href="mailto: kurtien@live.no">Kurtien@live.no</a>
+        <a class="white" href="mailto:kurtien@live.no">Kurtien@live.no</a>
     </div>
     
 
